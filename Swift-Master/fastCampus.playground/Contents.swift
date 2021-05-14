cc791f16e2448a7f3df28da70bfdbba6f10f41a7
youngmo <youngmo0806@naver.com>
2021-05-14 19:11:06 +0900

    2021/05/14:first

--- Swift-Master/fastCampus.playground/Contents.swift
+++ Swift-Master/fastCampus.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-//function
+//MARK: - function
 // 1. 성, 이름을 받아서 fullName을 출력하는 함수 만들기
 func namePrint(firstName: String, lastName: String) {
     print("full name is : \(firstName) \(lastName)")
@@ -14,17 +14,30 @@ func namePrint2(_ firstName: String, _ lastName: String) {
 // 3. 성, 이름을 받아서 fullname return 하는 함수 만들기
 
 func namePrint3(firstName: String, lastName: String) -> String{
-    var fullname = "\(firstName) \(lastName)"
+    let fullname = "\(firstName) \(lastName)"
     return fullname
 }
-
-
-
-
-
 
 //exec
 namePrint(firstName: "youngmo", lastName: "jung")
 namePrint2("youngmo", "jung")
 var result = namePrint3(firstName: "youngmo", lastName: "jung")
 print("\(result)")
+
+
+
+//MARK: - Optional
+var carName: String?
+var actor: String? = "iu"
+
+//캐스팅이 될수도 안될수도 있기 때문에 Int? 타입이 된다.
+let num = Int("10")
+
+//Optional 고급기능 4가지
+
+//Forced unrapping ->
+//Optional binding (if let)
+//Optional binding (guard)
+//Nil coalescing
+
+//940909
