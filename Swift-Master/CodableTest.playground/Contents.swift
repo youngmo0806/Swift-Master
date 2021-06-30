edc75b5765fd07e6c1049e6a32e1dd797429213f
youngmo <youngmo0806@naver.com>
2021-06-30 14:20:54 +0900

    2021/06/30:Second

--- Swift-Master/CodableTest.playground/Contents.swift
+++ Swift-Master/CodableTest.playground/Contents.swift
@@ -1,3 +1,9 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//Codable
+//자신을 변환하거나 외부표현으로 변환할 수 있는 타입입니다.
+
+//typealias Codable = Decodable & Encodable
+//protocol Decodable
+//protocol Encodable
+
