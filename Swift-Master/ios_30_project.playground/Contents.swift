a8b1371e67e2bdd5d770624a96a4772526a5cd74
youngmo <youngmo>
2021-08-23 17:22:42 +0900

    2021/08/23:First

---
+++ Swift-Master/ios_30_project.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+print("TEST")
+//
+////assert
+//var value = 0
+//assert(value == 0)
+//
+//value = 2
+//
+//assert(value == 0, "값이 0이 아닙니다.")
+//
+//
+////guard
+//func guardTest(value: Int) {
+//    guard value == 0  else {  return }
+//    print("안녕하세요")
+//}
+//
+//guardTest(value: 2)
+//guardTest(value: 0)
