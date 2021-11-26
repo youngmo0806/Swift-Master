6dd02c1a80bb00724af9e7e756ea0226b617eb13
youngmo <youngmo0806@naver.com>
2021-11-26 10:23:29 +0900

    random func add.

---
+++ Swift-Master/swift_random_Test.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+//Random Func Test Module
+
+//Swift에서 제공하는 랜덤함수 세가지
+
+var test1 = arc4random() //0과 2^32-1 사이의 난수
+print("arc4random -> UInt32 return => [\(test1)]")
+
+var test2 = arc4random_uniform(500) //0~499 사이의 난수
+print("arc4random_uniform -> UInt32 return => [\(test2)]")
+
+var test3 = drand48() //0~0.1 사이의 난수
+print("drand48 -> Double return => [\(test3)]")
