5a52a04dd608f622cd5bc74ab64a2c2f1dfb45ff
youngmo <youngmo0806@naver.com>
2022-08-05 13:50:36 +0900

    CodeTesting

---
+++ Swift-Master/CodeTest.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+import Foundation
+
+
+var input:[Int] = [1,2,3,4]
+
+solution(input)
+
+
+func solution(_ nums:[Int]) -> Int {
+    var answer = -1
+
+    for num in nums {
+        print(num)
+        
+    }
+    
+    
+
+    return answer
+}
+
+
