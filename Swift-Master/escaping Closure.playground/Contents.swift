1b0152c694cd622173845b584d043170d9b03015
youngmo <youngmo0806@naver.com>
2023-01-13 18:26:14 +0900

    23.01.13.18:26

--- Swift-Master/escaping Closure.playground/Contents.swift
+++ Swift-Master/escaping Closure.playground/Contents.swift
@@ -59,3 +59,53 @@ func appendClosure(closure: @escaping VoidVoidClosure) {
 
 
 
+/// Non-Escaping Closure
+/// - Parameter closure: runClosure() 함수의 closure 인자로 전달됨
+func runClosure(closure: () -> Void) {
+    closure()
+}
+
+//클로저가 실행되는 순서를 보면
+//
+//클로저가 runClosure() 함수의 closure 인자로 전달됨
+//함수 안에서 closure() 가 실행됨
+//runClosure() 함수가 값을 반환하고 종료됨
+//이렇게 클로저가 함수가 종료되기 전에 실행되기 때문에 closure는 Non-Escaping 클로저 입니다.
+
+
+
+
+class ViewModel {
+    var completionhandler: (() -> Void)? = nil
+    
+    /// Escaping Closure
+    /// - Parameter completion:fetchData() 함수의 completion 인자로 전달됨
+    func fetchData(completion: @escaping () -> Void) {
+        completionhandler = completion
+    }
+}
+
+//클로저가 실행되는 순서를 보면
+//
+//클로저가 fetchData() 함수의 completion 인자로 전달됨
+//클로저 completion이 completionhandler 변수에 저장됨
+//fetchData() 함수가 값을 반환하고 종료됨
+//클로저 completion은 아직 실행되지 않음
+//completion은 함수의 실행이 종료되기 전에 실행되지 않기 때문에 escaping 클로저, 다시말해 함수 밖(escaping)에서 실행되는 클로저 입니다.
+
+
+
+//escaping 클로저가 사용되는 흔한 예로는 비동기로 실행되는 HTTP Request CompletionHandler이 있습니다.
+func makeRequest(_ completion: @escaping (Result<(Data, URLResponse), Error>) -> Void ) {
+    URLSession.shared.dataTask(with: URL(string: "http://jusung.github.io/")!) { data, response, error in
+      if let error = error {
+        completion(.failure(error))
+      } else if let data = data, let response = response {
+        completion(.success((data, response)))
+      }
+    }
+}
+
+//makeRequest() 함수에서 사용되는 completion 클로저는 함수 실행 중에 즉시 실행되지 않고,
+//URL 요청이 끝난 후 비동기로 실행 됩니다. 이 경우에도 completion의 타입에 @escaping 을 붙여서 escaping 클로저라는 것을 명시해줘야 합니다.
+//보통 클로저가 다른 변수에 저장되어 나중에 실행되거나 비동기로 실행될 때 escaping 클로저가 사용됩니다.
